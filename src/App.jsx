efa74e802b8ca9dea606e016ffe4be4cabeec4e0
Francisco Capone <francisco.capone@gmail.com>
2024-09-13 20:27:36 -0300

    Primera versión plantilla React Traffic Light

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Semaforo } from './Semaforo';
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Semaforo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Semaforo.jsx
@@ -0,0 +1,11 @@
+import './Semaforo.css';
+
+export const Semaforo = () => {
+  return (
+    <div id="semaforo">
+      <div className="luz roja"></div>
+      <div className="luz amarilla"></div>
+      <div className="luz verde"></div>
+    </div>
+  )
+};

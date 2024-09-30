19503de8a3390a65bb1972a51746ca0173666a93
Carlos <digiampaolo@outlook.com>
2024-09-30 18:01:01 -0300

    first

--- src/Semaforo.jsx
+++ src/Semaforo.jsx
@@ -1,11 +1,17 @@
+import React, { useState } from 'react';
 import './Semaforo.css';
 
 export const Semaforo = () => {
+
+  const [color, setColor] = useState('red'); // Estado INICIAL
+
+  const handelClick = (nuevoColor) => { setColor(nuevoColor)};
+  
   return (
     <div id="semaforo">
-      <div className="luz roja"></div>
-      <div className="luz amarilla"></div>
-      <div className="luz verde"></div>
+      <div className={`luz roja ${color === 'red' ? 'activa' : ''}`} onClick={() => handelClick('red')}></div>
+      <div className={`luz amarilla ${color === 'yellow' ? 'activa' : ''}`} onClick={() => handelClick('yellow')}></div>
+      <div className={`luz verde ${color === 'green' ? 'activa' : ''}`} onClick={() => handelClick('green')}></div>
     </div>
-  )
+  );
 };
